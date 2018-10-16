777d34f88acc20ed56a302f80571dcf8eb54c56b
Gabriel Nikol <GabrielNikol@web.de>
2018-10-16 20:41:34 +0200

    fixed: System::exit(), parsing on various platforms and added F(...) to Strings

--- TransmissionProtocolUartBridge.h
+++ TransmissionProtocolUartBridge.h
@@ -1,7 +1,7 @@
 /*
 * The MIT License (MIT)
 *
-* Copyright (C) 2018 Copyright (c) 2018 Gabriel Nikol
+* Copyright (C) 2018 Copyright Gabriel Nikol
 */
 
 #ifndef ARDUINO_MQTTSN_CLIENT_TRANSMISSIONPROTOCOLUARTBRDIGE_H
@@ -12,6 +12,8 @@
 #include <stdint.h>
 #include "global_defines.h"
 #include "mqttsn_messages.h"
+#include <Stream.h>
+
 
 #if defined(ESP8266)
 #include <cerrno>
@@ -59,9 +61,11 @@ enum CONFIGURATION_STATUS {
     CONFIGURATION_SERIAL_BUFFER_SIZE = 5
 };
 
-
-#define SerialBufferSize 300
-
+// #define PARSERDATADEBUG
+// #define PARSERADDRESSDEBUG
+#define SERIAL_BUFFER_SIZE 200
+#define RECEIVE_BUFFER_SIZE 64
+#define SEND_BUFFER_SIZE 64
 
 template<class TransmissionProtocolUartBridge_SocketInterface>
 class TransmissionProtocolUartBridge {
@@ -77,24 +81,26 @@ private:
     uint8_t configuration_status = CONFIGURATION_NONE;
 
 
-    // SerialBuffer
+    // StreamBuffer
     bool lineReady = false;
-    char serialBuffer[SerialBufferSize];
+    char serialBuffer[SERIAL_BUFFER_SIZE];
     uint16_t serialBufferCounter = 0;
 
     // SendBuffer
     device_address destination_address;
-    uint8_t data[64];
+    uint8_t data[SEND_BUFFER_SIZE];
     uint16_t data_length = 0;
 
     // ReceiveBuffer
     device_address receive_address;
-    uint8_t receive_buffer[64];
+    uint8_t receive_buffer[RECEIVE_BUFFER_SIZE];
     uint16_t receive_buffer_length = 0;
+    Stream *stream;
 
 public:
-    explicit TransmissionProtocolUartBridge(TransmissionProtocolUartBridge_SocketInterface &socketInterface) :
-            socketInterface(socketInterface) {}
+    explicit TransmissionProtocolUartBridge(Stream *stream,
+                                            TransmissionProtocolUartBridge_SocketInterface &socketInterface) :
+            stream(stream), socketInterface(socketInterface) {}
 
     bool begin() {
         memset(serialBuffer, 0x0, sizeof(serialBuffer));
@@ -108,18 +114,24 @@ public:
     }
 
     /*
-     * Receive from Serial/UART
+     * Receive from Stream
      */
     void putChar(char c) {
         serialBuffer[serialBufferCounter++] = c;
         if (c == '\n') {
             lineReady = true;
-        } else if (serialBufferCounter == SerialBufferSize) {
+        } else if (serialBufferCounter == SERIAL_BUFFER_SIZE) {
             status = PARSE_FAILURE;
         }
     }
 
     void receiveData(device_address *address, uint8_t *bytes, uint16_t bytes_length) {
+        reset_received_buffer();
+
+        memcpy(&receive_address, address, sizeof(device_address));
+        receive_buffer_length = bytes_length;
+        memcpy(receive_buffer, bytes, bytes_length);
+        /*
         if (bytes[0] != bytes_length) {
             return;
         }
@@ -132,25 +144,26 @@ public:
             receive_buffer_length = bytes[0];
             memcpy(receive_buffer, bytes, receive_buffer_length);
         }
+         */
     }
 
     bool print_received_address() {
-        Serial.print("ADDRESS");
+        stream->print(F("ADDRESS"));
         for (uint16_t i = 0; i < sizeof(device_address); i++) {
-            Serial.print(" ");
-            Serial.print(receive_address.bytes[i], DEC);
+            stream->print(F(" "));
+            stream->print(receive_address.bytes[i], DEC);
         }
-        Serial.print("\n");
+        stream->print(F("\n"));
         return true;
     }
 
     bool print_received_data() {
-        Serial.print("DATA");
+        stream->print(F("DATA"));
         for (uint16_t i = 0; i < receive_buffer_length; i++) {
-            Serial.print(" ");
-            Serial.print(receive_buffer[i], DEC);
+            stream->print(F(" "));
+            stream->print(receive_buffer[i], DEC);
         }
-        Serial.print("\n");
+        stream->print(F("\n"));
         return true;
     }
 
@@ -166,14 +179,16 @@ public:
 
     void resetChip() {
 #if defined(ESP8266)
-        Serial.print(F("OK RESET\n"));
+        stream->print(F("OK RESET\n"));
+        delay(500);
         ESP.restart();
 #elif defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_YUN)
-        Serial.print(F("OK RESET\n"));
-        asm volatile ("  jmp 0");  
+        stream->print(F("OK RESET\n"));
+        delay(500);
+        asm volatile ("  jmp 0");
 #else
-        #warning "resetChip() is not properly implemented. This means we cannot reset the TransmissionProtocolUartBridge."
-        Serial.print(F("ERROR RESET_NOT_SUPPORTED\n"));
+#warning "resetChip() is not properly implemented. This means we cannot reset the TransmissionProtocolUartBridge."
+        stream->print(F("ERROR RESET_NOT_SUPPORTED\n"));
 #endif
     }
 
@@ -192,7 +207,7 @@ public:
 
         if (status == STARTING) {
             // begin should be already called
-            Serial.print("ERROR NOT_STARTED\n");
+            stream->print(F("ERROR NOT_STARTED\n"));
             return false;
         }
 
@@ -221,12 +236,12 @@ public:
 
         if (status == SEND) {
             if (send_status == SEND_NONE) {
-                Serial.print("OK AWAIT_ADDRESS\n");
+                stream->print(F("OK AWAIT_ADDRESS\n"));
                 resetSendBuffer();
                 send_status = AWAIT_ADDRESS;
             } else if (send_status == AWAIT_ADDRESS && lineReady) {
                 if (parseAddress(serialBuffer)) {
-                    Serial.print("OK AWAIT_DATA\n");
+                    stream->print(F("OK AWAIT_DATA\n"));
                     resetSerialBuffer();
                     send_status = AWAIT_DATA;
                 } else {
@@ -234,7 +249,7 @@ public:
                 }
             } else if (send_status == AWAIT_DATA && lineReady) {
                 if (parseData(serialBuffer)) {
-                    Serial.print("OK SENDING\n");
+                    stream->print(F("OK SENDING\n"));
                     resetSerialBuffer();
                     send_status = SENDING;
                 } else {
@@ -242,7 +257,7 @@ public:
                 }
             } else if (send_status == SENDING) {
                 if (sendDataToAddress()) {
-                    Serial.print("OK IDLE\n");
+                    stream->print(F("OK IDLE\n"));
                     resetSendBuffer();
                     send_status = SEND_NONE;
                     status = IDLE;
@@ -254,18 +269,18 @@ public:
 
         if (status == RECEIVE) {
             if (receive_status == RECEIVE_NONE) {
-                Serial.print("OK SEND_ADDRESS\n");
+                stream->print(F("OK SEND_ADDRESS\n"));
                 receive_status = SEND_ADDRESS;
             } else if (receive_status == SEND_ADDRESS) {
                 if (send_receive_address()) {
-                    Serial.print("OK SEND_DATA\n");
+                    stream->print(F("OK SEND_DATA\n"));
                     receive_status = SEND_DATA;
                 } else {
                     status = ERROR;
                 }
             } else if (receive_status == SEND_DATA) {
                 if (send_receive_data()) {
-                    Serial.print("OK IDLE\n");
+                    stream->print(F("OK IDLE\n"));
                     resetReceiveBuffer();
                     receive_status = RECEIVE_NONE;
                     status = IDLE;
@@ -277,39 +292,39 @@ public:
 
         if (status == CONFIGURATION) {
             if (configuration_status == CONFIGURATION_NONE) {
-                Serial.print("OK SEND_STATUS\n");
+                stream->print(F("OK SEND_STATUS\n"));
                 configuration_status = CONFIGURATION_STATUS;
             } else if (configuration_status == CONFIGURATION_STATUS) {
                 if (printStatus()) {
-                    Serial.print("OK SEND_OWN_ADDRESS\n");
+                    stream->print(F("OK SEND_OWN_ADDRESS\n"));
                     configuration_status = CONFIGURATION_OWN_ADDRESS;
                 } else {
                     status = ERROR;
                 }
             } else if (configuration_status == CONFIGURATION_OWN_ADDRESS) {
                 if (printOwnAddress()) {
-                    Serial.print("OK SEND_BROADCAST_ADDRESS\n");
+                    stream->print(F("OK SEND_BROADCAST_ADDRESS\n"));
                     configuration_status = CONFIGURATION_BROADCAST_ADDRESS;
                 } else {
                     status = ERROR;
                 }
             } else if (configuration_status == CONFIGURATION_BROADCAST_ADDRESS) {
                 if (printBroadcastAddress()) {
-                    Serial.print("OK SEND_MAXIMUM_MESSAGE_LENGTH\n");
+                    stream->print(F("OK SEND_MAXIMUM_MESSAGE_LENGTH\n"));
                     configuration_status = CONFIGURATION_MAXIMUM_MESSAGE_LENGTH;
                 } else {
                     status = ERROR;
                 }
             } else if (configuration_status == CONFIGURATION_MAXIMUM_MESSAGE_LENGTH) {
                 if (printMaximumMessageLength()) {
-                    Serial.print("OK SEND_SERIAL_BUFFER_SIZE\n");
+                    stream->print(F("OK SEND_SERIAL_BUFFER_SIZE\n"));
                     configuration_status = CONFIGURATION_SERIAL_BUFFER_SIZE;
                 } else {
                     status = ERROR;
                 }
             } else if (configuration_status == CONFIGURATION_SERIAL_BUFFER_SIZE) {
                 if (printSerialBufferSize()) {
-                    Serial.print("OK IDLE\n");
+                    stream->print(F("OK IDLE\n"));
                     configuration_status = CONFIGURATION_NONE;
                     status = IDLE;
                 } else {
@@ -319,7 +334,7 @@ public:
         }
 
         if (status == PARSE_FAILURE) {
-            Serial.print("FAILURE PARSE_FAILURE\n");
+            stream->print(F("FAILURE PARSE_FAILURE\n"));
             resetSerialBuffer();
             receive_status = RECEIVE_NONE;
             send_status = SEND_NONE;
@@ -327,7 +342,7 @@ public:
         }
         /*
         if (status == FAILURE) {
-            Serial.print("FAILURE\n");
+            stream->print(F("FAILURE\n"));
             resetSerialBuffer();
             receive_status = RECEIVE_NONE;
             send_status = SEND_NONE;
@@ -335,7 +350,7 @@ public:
         }
         */
         if (status == ERROR) {
-            Serial.print("ERROR\n");
+            stream->print(F("ERROR\n"));
             resetSerialBuffer();
             resetChip();
         }
@@ -343,74 +358,74 @@ public:
     }
 
     bool printStatus() {
-        Serial.print("STATUS");
-        Serial.print(" ");
+        stream->print(F("STATUS"));
+        stream->print(F(" "));
         if (status == STARTING) {
-            Serial.print("STARTING");
+            stream->print(F("STARTING"));
         } else if (status == IDLE) {
-            Serial.print("IDLE");
+            stream->print(F("IDLE"));
         } else if (status == SEND) {
-            Serial.print("SEND");
+            stream->print(F("SEND"));
         } else if (status == CONFIGURATION) {
-            Serial.print("CONFIGURATION");
+            stream->print(F("CONFIGURATION"));
         } else if (status == PARSE_FAILURE) {
-            Serial.print("PARSE_FAILURE");
+            stream->print(F("PARSE_FAILURE"));
         } else if (status == ERROR) {
-            Serial.print("ERROR");
+            stream->print(F("ERROR"));
         } else {
-            Serial.print("\n");
+            stream->print(F("\n"));
             return false;
         }
-        Serial.print("\n");
+        stream->print(F("\n"));
         return true;
     }
 
     bool printOwnAddress() {
-        Serial.print("OWN_ADDRESS");
+        stream->print(F("OWN_ADDRESS"));
         device_address *address = socketInterface.getAddress();
         if (address != nullptr) {
             for (uint16_t i = 0; i < sizeof(device_address); i++) {
-                Serial.print(" ");
-                Serial.print(address->bytes[i], DEC);
+                stream->print(F(" "));
+                stream->print(address->bytes[i], DEC);
             }
         } else {
-            Serial.print(" ");
-            Serial.print("N/A");
+            stream->print(F(" "));
+            stream->print(F("N/A"));
         }
-        Serial.print("\n");
+        stream->print(F("\n"));
         return true;
     }
 
     bool printBroadcastAddress() {
-        Serial.print("BROADCAST_ADDRESS");
+        stream->print(F("BROADCAST_ADDRESS"));
         device_address *address = socketInterface.getBroadcastAddress();
         if (address != nullptr) {
             for (uint16_t i = 0; i < sizeof(device_address); i++) {
-                Serial.print(" ");
-                Serial.print(address->bytes[i], DEC);
+                stream->print(F(" "));
+                stream->print(address->bytes[i], DEC);
             }
         } else {
-            Serial.print(" ");
-            Serial.print("N/A");
+            stream->print(F(" "));
+            stream->print(F("N/A"));
         }
-        Serial.print("\n");
+        stream->print(F("\n"));
         return true;
     }
 
     bool printMaximumMessageLength() {
-        Serial.print("MAXIMUM_MESSAGE_LENGTH");
-        Serial.print(" ");
+        stream->print(F("MAXIMUM_MESSAGE_LENGTH"));
+        stream->print(F(" "));
         uint8_t maximumMessageLength = socketInterface.getMaximumMessageLength();
-        Serial.print(maximumMessageLength, DEC);
-        Serial.print("\n");
+        stream->print(maximumMessageLength, DEC);
+        stream->print(F("\n"));
         return true;
     }
 
     bool printSerialBufferSize() {
-        Serial.print("SERIAL_BUFFER_SIZE");
-        Serial.print(" ");
-        Serial.print(SerialBufferSize, DEC);
-        Serial.print("\n");
+        stream->print(F("SERIAL_BUFFER_SIZE"));
+        stream->print(F(" "));
+        stream->print(SERIAL_BUFFER_SIZE, DEC);
+        stream->print(F("\n"));
         return true;
     }
 
@@ -418,10 +433,10 @@ public:
     void printSerialBuffer() {
         if (lineReady) {
             for (uint16_t i = 0; i < serialBufferCounter; i++) {
-                Serial.print(serialBuffer[i]);
+                stream->print(serialBuffer[i]);
             }
         } else {
-            Serial.print("lineNotReady");
+            stream->print(F("lineNotReady"));
         }
     }
 
@@ -441,7 +456,7 @@ public:
     }
 
     bool send_error() {
-        Serial.print("ERROR\n");
+        stream->print(F("ERROR\n"));
         return true;
     }
 
@@ -470,11 +485,20 @@ public:
     }
 
     bool parseData(char *buffer) {
+#if defined(PARSERDATADEBUG)
+        stream->print(F("parseData\n"));
+#endif
         char *token = strsep(&buffer, " ");
         if (token == NULL) {
+#if defined(PARSERDATADEBUG)
+            stream->print(F("token NULL\n"));
+#endif
             return false;
         }
         if (memcmp(token, "DATA", strlen("DATA")) != 0) {
+#if defined(PARSERDATADEBUG)
+            stream->print(F("does not start with DATA\n"));
+#endif
             return false;
         }
 
@@ -484,32 +508,40 @@ public:
         while ((token = strsep(&buffer, " ")) != NULL) {
             long int number = 0;
             if (!parseLong(token, &number)) {
+#if defined(PARSERDATADEBUG)
+                stream->print(F("failure parsing parseLong\n"));
+#endif
                 return false;
             }
             if (number > UINT8_MAX || number < 0) {
+#if defined(PARSERDATADEBUG)
+                stream->print(F("number out of range\n"));
+#endif
                 return false;
             }
+#if defined(PARSERDATADEBUG)
+            stream->print(number, DEC);
+            stream->print(F("\n"));
+#endif
             data[data_length++] = (uint8_t) number;
         }
         return true;
     }
 
-//#define PARSERADDRESSDEBUG
-
     bool parseAddress(char *buffer) {
 #if defined(PARSERADDRESSDEBUG)
-        Serial.print("parseAddress\n");
+        stream->print(F("parseAddress\n"));
 #endif
         char *token = strsep(&buffer, " ");
         if (token == NULL) {
 #if defined(PARSERADDRESSDEBUG)
-            Serial.print("token NULL\n");
+            stream->print(F("token NULL\n"));
 #endif
             return false;
         }
         if (memcmp(token, "ADDRESS", strlen("ADDRESS")) != 0) {
 #if defined(PARSERADDRESSDEBUG)
-            Serial.print("does not start with ADDRESS\n");
+            stream->print(F("does not start with ADDRESS\n"));
 #endif
             return false;
         }
@@ -521,30 +553,35 @@ public:
             long int number = 0;
             if (!parseLong(token, &number)) {
 #if defined(PARSERADDRESSDEBUG)
-                Serial.print("failure parsing parseLong\n");
+                stream->print(F("failure parsing parseLong\n"));
 #endif
                 return false;
             }
 #if defined(PARSERADDRESSDEBUG)
-            Serial.print(number, DEC);
-    Serial.print("\n");
+            stream->print(number, DEC);
+            stream->print(F("\n"));
 #endif
             if (number > UINT8_MAX || number < 0) {
 #if defined(PARSERADDRESSDEBUG)
-                Serial.print("number out of bounds: ");
-      Serial.print(number, DEC);
-      Serial.print("\n");
+                stream->print(F("number out of bounds: "));
+                stream->print(number, DEC);
+                stream->print(F("\n"));
 #endif
                 return false;
             }
             if (destination_address_length + 1 > sizeof(device_address)) {
 #if defined(PARSERADDRESSDEBUG)
-                Serial.print("address size too long");
+                stream->print(F("address size too long"));
 #endif
                 return false;
             }
             destination_address.bytes[destination_address_length++] = (uint8_t) number;
         }
+#if defined(PARSERADDRESSDEBUG)
+        if (destination_address_length != sizeof(device_address)) {
+            stream->print(F("destination_address_length unequal device_address length"));
+        }
+#endif
         return destination_address_length == sizeof(device_address);
     }
 
@@ -581,15 +618,16 @@ public:
 #endif
         *val = strtol(str, &temp, 0);
 
-        if (temp == str || (*temp != '\0' && *temp != '\n') ||
-#if defined(ESP8266)
+        if (temp == str || (*temp != '\0' && *temp != '\n' && *temp != '\r') ||
+            #if defined(ESP8266)
             ((*val == LONG_MIN || *val == LONG_MAX) && errno == ERANGE))
-#else
+            #else
             // see: strtol() function return values
             ((*val == LONG_MIN || *val == LONG_MAX) && true))
 #endif
+        {
             rc = false;
-
+        }
         return rc;
     }
 
